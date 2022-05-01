235bc02955f0ea03fd34f8546b6ea8e8efd6f5f9
Jhon Sebastian Bohoruez Guerrero <js.bohorquezg1@uniandes.edu.co>
2022-05-01 00:53:10 -0500

    Pruebas finalizadas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VehiculoListaComponent } from './vehiculos/vehiculo-lista/vehiculo-lista.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [AppComponent, VehiculoListaComponent],
     }).compileComponents();
   });
 
@@ -20,14 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('base-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'base-project app is running!'
-    );
   });
 });
--- src/app/vehiculos/vehiculo-lista/vehiculo-lista.component.spec.ts
+++ src/app/vehiculos/vehiculo-lista/vehiculo-lista.component.spec.ts
@@ -2,16 +2,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 
 import { VehiculoListaComponent } from './vehiculo-lista.component';
+import { VehiculoService } from '../vehiculo.service';
+import { Vehiculo } from '../vehiculo';
 
 describe('VehiculoListaComponent', () => {
   let component: VehiculoListaComponent;
   let fixture: ComponentFixture<VehiculoListaComponent>;
+  let debug: DebugElement;
+
+  var table = document.getElementById("tablaPrincipal");
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehiculoListaComponent ]
+      imports: [HttpClientModule],
+      declarations: [ VehiculoListaComponent ],
+      providers: [VehiculoService]
     })
     .compileComponents();
   }));
@@ -19,10 +28,52 @@ describe('VehiculoListaComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListaComponent);
     component = fixture.componentInstance;
+
+    component.vehiculos = [
+      new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      ),
+      new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      ),
+      new Vehiculo(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      )
+    ]
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have four rows', () => {
+    var table = document.getElementById("tablaPrincipal") as HTMLTableElement;
+    var tableRows = table.rows.length;
+
+    expect(tableRows).toEqual(4);
+  })
 });
